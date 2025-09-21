e6a6426624f8332a5e6daf87410ea993739e7cf9
Dinura Ransika <ransikadinura@gmail.com>
2025-09-21 22:57:20 +0530

    feat: import landing viewing data

--- resources/js/pages/Landing.tsx
+++ resources/js/pages/Landing.tsx
@@ -7,6 +7,9 @@ import InformationSection from "@/components/sections/InformationSection";
 import BlogSection from "@/components/sections/BlogSection";
 import Footer from "@/components/sections/Footer";
 
+//  Import data form the constants folder
+import { descriptionItems, informationItems } from "@/constants/landing-data";
+
 interface Project {
   id: number;
   name: string;
@@ -29,44 +32,6 @@ interface LandingProps {
 }
 
 export default function Landing({ projects, blogs }: LandingProps) {
-  // Description section data
-  const descriptionItems = [
-    {
-      id: 1,
-      title: "Lorem ipsum dolor sit amet consectetur.",
-      content: [
-        "Lorem ipsum dolor sit amet consectetur. Felis arcu sed diam sit quis fermentum.",
-        "Lacinia neque aliquam facilisis sceleris magna. Sed et tellus sapien rutrum egestas pellentesque rutrum ut.",
-        "Lorem ipsum dolor sit amet consectetur. Pellentesque nunc mauris dictum sodales quis porta."
-      ]
-    }
-  ];
-
-  // Information section data
-  const informationItems = [
-    {
-      id: 1,
-      number: "25",
-      title: "Lorem ipsum dolor sit amet consectetur.",
-      buttonText: "Press release",
-      buttonLink: "/press-release-1"
-    },
-    {
-      id: 2,
-      number: "25",
-      title: "Lorem ipsum dolor sit amet consectetur.",
-      buttonText: "Press release",
-      onClick: () => console.log("Information item 2 clicked")
-    },
-    {
-      id: 3,
-      number: "25",
-      title: "Lorem ipsum dolor sit amet consectetur.",
-      buttonText: "Press release",
-      buttonLink: "/press-release-3"
-    }
-  ];
-
   return (
     <div className="bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
       <Head title="Agency Inc. - Creative Digital Solutions" />
@@ -79,7 +44,7 @@ export default function Landing({ projects, blogs }: LandingProps) {
         showCards={true}
         ctaButtons={{
           primary: { text: "Get Started" },
-          secondary: { text: "Learn More" }
+          secondary: { text: "Learn More" },
         }}
       />
 
@@ -100,6 +65,7 @@ export default function Landing({ projects, blogs }: LandingProps) {
         titlePosition="left"
         showArrow={true}
       />
+
       <Footer />
     </div>
   );
