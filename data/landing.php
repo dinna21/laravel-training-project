7f126f2afe9504e700f9112375c7865b80dc299e
Dinura Ransika <ransikadinura@gmail.com>
2025-09-25 23:38:00 +0530

    feat: create the php array data that include landing page data that displayed

---
+++ data/landing.php
@@ -0,0 +1,86 @@
+<?php
+return [
+    'projects' => [
+        [
+            'id' => 1,
+            'name' => 'E-Commerce Platform',
+            'description' => 'Modern online shopping platform with advanced features.',
+            'image' => '/images/office--img.jpg',
+            'category' => 'Web Development',
+            'technologies' => ['React', 'Laravel', 'MySQL']
+        ],
+        [
+            'id' => 2,
+            'name' => 'Mobile Banking App',
+            'description' => 'Secure and user-friendly banking application.',
+            'image' => '/images/office--img.jpg',
+            'category' => 'Mobile App',
+            'technologies' => ['React Native', 'Node.js', 'MongoDB']
+        ],
+        [
+            'id' => 3,
+            'name' => 'Corporate Website',
+            'description' => 'Professional corporate website with CMS integration.',
+            'image' => '/images/office--img.jpg',
+            'category' => 'Web Design',
+            'technologies' => ['Next.js', 'Tailwind CSS', 'Strapi']
+        ],
+    ],
+
+    'blogs' => [
+        [
+            'id' => 1,
+            'title' => 'Understanding Inertia.js',
+            'description' => 'A deep dive into building modern web applications with Inertia.js and Laravel.',
+            'image' => '/images/office--img.jpg',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Getting Started with React',
+            'description' => 'An introductory guide to building applications with React.',
+            'image' => '/images/office--img.jpg',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Laravel Best Practices',
+            'description' => 'Tips and tricks for writing clean and efficient Laravel code.',
+            'image' => '/images/office--img.jpg',
+        ],
+    ],
+
+    'descriptionItems' => [
+        [
+            'id' => 1,
+            'title' => 'Lorem ipsum dolor sit amet consectetur.',
+            'content' => [
+                'Lorem ipsum dolor sit amet consectetur. Felis arcu sed diam sit quis fermentum.',
+                'Lacinia neque aliquam facilisis sceleris magna. Sed et tellus sapien rutrum egestas pellentesque rutrum ut.',
+                'Lorem ipsum dolor sit amet consectetur. Pellentesque nunc mauris dictum sodales quis porta.',
+            ],
+        ],
+    ],
+
+    'informationItems' => [
+        [
+            'id' => 1,
+            'number' => '25',
+            'title' => 'Lorem ipsum dolor sit amet consectetur.',
+            'buttonText' => 'Press release',
+            'buttonLink' => '/press-release-1',
+        ],
+        [
+            'id' => 2,
+            'number' => '25',
+            'title' => 'Lorem ipsum dolor sit amet consectetur.',
+            'buttonText' => 'Press release',
+            'buttonLink' => '/press-release-2',
+        ],
+        [
+            'id' => 3,
+            'number' => '25',
+            'title' => 'Lorem ipsum dolor sit amet consectetur.',
+            'buttonText' => 'Press release',
+            'buttonLink' => '/press-release-3',
+        ],
+    ],
+];
