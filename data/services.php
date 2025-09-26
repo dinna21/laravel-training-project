8c89959e75f23290316383241b0a2a4ee6b7d28b
Dinura Ransika <ransikadinura@gmail.com>
2025-09-26 16:42:24 +0530

    feat: craete array which display in the services page

---
+++ data/services.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    'services' => [
+        [
+            'id' => 1,
+            'title' => 'UX/UI Design & Research',
+            'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus. At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
+            'badges' => ['UX/UI Design', 'Web Design', 'Native App'],
+            'projects' => [
+                [
+                    'id' => 1,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-1.jpg'
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-1.jpg'
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-1.jpg'
+                ]
+            ]
+        ],
+        [
+            'id' => 2,
+            'title' => 'Design System',
+            'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus. At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
+            'badges' => ['Components', 'Guidelines'],
+            'projects' => [
+                [
+                    'id' => 4,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-2.png'
+                ],
+                [
+                    'id' => 5,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-1.jpg'
+                ],
+                [
+                    'id' => 6,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-2.png'
+                ]
+            ]
+        ],
+        [
+            'id' => 3,
+            'title' => 'Web Design & Nocode Development',
+            'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus. At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
+            'badges' => ['UX/UI Design', 'Web Design', 'Native App'],
+            'projects' => [
+                [
+                    'id' => 1,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-2.png'
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-1.jpg'
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Project Name',
+                    'image' => '/images/project-2.png'
+                ]
+            ]
+        ]
+    ]
+];
