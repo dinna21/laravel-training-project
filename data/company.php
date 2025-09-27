5567ed47bfb2ea83b04ad24922df0c15477e5838
Dinura Ransika <ransikadinura@gmail.com>
2025-09-28 00:41:01 +0530

    feat: add the company data in the pah array

---
+++ data/company.php
@@ -0,0 +1,45 @@
+<?php
+// filepath: e:\laravel-training-project\data\company.php
+
+return [
+    'company' => [
+        'title' => 'Lorem ipsum dolor sit amet consectetur.',
+        'description' => [
+            'Lorem ipsum dolor sit amet consectetur. Et interdum arcu gravida urna viverra bibendum habitant placerat nec justo mollis. Quis elegiaque pellentesque vitae et arcu interdum viam lectus. Dictum imperdiet quam eu amet. Vivamus felis sollicituaque et id non ut. Etiam congue consectetur nulla amet et. Nisl ut eros risique quam pellentesque nunc sed.',
+            'Nunc omcare sit aliquet arroe omcare molestiae risus vel blandit. Velit incididunt tempus vel modi gravida eget malesanda solitudinis. Ut et sed molestie vehicula defendit. Faucibus suscipit sed mattis risl et venenatis maecenas volutpat. Aliquam tempus velit quis sed libero sem nunc. Phasellut gravida neque tristique enim. Curabitur montes sed molestae tacuila. Velit augue proin nis nunc at amet non in pharetra. A vitrue arco consectetur risus enim eget quis sit. In viverra proin risus augue aem. Nec sed et enim et eget. Suscipit tristique vivamus vulputate donce.'
+        ],
+        'image' => '/images/project-2.png'
+    ],
+    'history' => [
+        [
+            'year' => '2020',
+            'month' => 'Nov',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ],
+        [
+            'year' => '2020',
+            'month' => 'Jul',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ],
+        [
+            'year' => '2020',
+            'month' => 'Dec',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ],
+        [
+            'year' => '2021',
+            'month' => 'Feb',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ],
+        [
+            'year' => '2022',
+            'month' => 'Dec',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ],
+        [
+            'year' => '2025',
+            'month' => 'Sep',
+            'description' => 'Lorem ipsum dolor sit amet consectetur.'
+        ]
+    ]
+];
