055df427dac56cf80cf90cf1dd754f41691c9e5e
Dinura Ransika <ransikadinura@gmail.com>
2025-09-30 13:15:57 +0530

    feat: add the data file that would be diplayed

---
+++ data/about.php
@@ -0,0 +1,25 @@
+<?php
+// filepath: e:\laravel-training-project\data\about.php
+
+return [
+    'project' => [
+        'name' => 'Projects Name',
+        'projectType' => 'UI Design System',
+        'timeline' => 'January 2023 - April 2023',
+        'client' => 'Lorem ipsum dolor'
+    ],
+    'about' => [
+        'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus. At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
+        'images' => [
+            '/images/project-1.jpg',
+            '/images/project-1.jpg'
+        ]
+    ],
+    'solutions' => [
+        'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus.',
+        'additionalInfo' => 'At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
+        'images' => [
+            '/images/project.jpg'
+        ]
+    ]
+];
