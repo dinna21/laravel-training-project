7e1bab832da18e765b77d37d4b035a5089cb45c8
Dinura Ransika <ransikadinura@gmail.com>
2025-10-02 14:58:04 +0530

    feat: add the blog page data whhich display

---
+++ data/blog.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'blog' => [
+        'title' => 'Lorem ipsum dolor sit amet consectetur.',
+        'author' => [
+            'name' => 'Shelby Daugherty',
+            'role' => 'Posted on June 13, 2023 4:11pm utc',
+            'avatar' => '/images/author-avatar.jpg',
+            'initials' => 'SD'
+        ],
+        'featuredImage' => '/images/office--img.jpg',
+        'content' => [
+            'Lorem ipsum dolor sit amet consectetur. Et interdum arcu gravida urna viverra blandit nam habitant placerat tortor. Quis quisque pellentesque viverra interdum orci lacus convallis at imperdiet quam vel sapien. Vivamus felis scelerisque ut non sit. Etiam congue consectetur nulla amet at. Nisl ut eros tristique nam adipiscing aliquet sed.',
+            'Nunc ornare et aliquet amet ornare molestie risus vel blandit. Velit tincidunt tempus vel nibh gravida eget malesuada sollicitudin. Id eget sed molestie ultrices eleifend. Faucibus suscipit sed mattis nisl et venenatis mauris volutpat. Aliquam tempus velit quis sed libero nisl nunc. Feugiat gravida neque tristique enim. Curabitur montes sed molestie iaculis. Velit augue proin nisl enim in amet non in pharetra. A tortor orci consectetur risus enim eget quis sit. In viverra proin netus augue sem. Nec sed et enim id orci. Suscipit tristique vivamus vulputate donec.',
+            'Lectus felis hac ac mi aenean. Nisl congue vulputate feugiat odio. Sed diam sodales quis mauris augue viverra arcu est. Odio dolor laoreet at viverra et scelerisque. Sit cursus sit nunc augue.',
+            'Eleifend ut pellentesque quis sit sapiens lectus hac sed. In neque in sit ullamcorper id pellentesque sit. Adipiscing integer vitae sapien convallis dictum. Fuming orci at euismod porttitor commodo et gravida mauris ac.',
+            'Lectus nunc rhoncus vitae consequat pharetra. Sapien facilisis porttitor imperdiet et tellus urna sed commodo porttitor arcu. Et sapien amet in dui cursus iaculis elit neque commodo. Tempor volutpat fringilla arcu dictus in eget fermentum ultricies nulla. Turpis odio ornare viverra placerat enim nibh. Mauris tincidunt malesuada egestas scelerisque lectus mauris cursus.',
+            'Arcu neque massa amet elit. Dui et volutpat pharetra tortor commodo mauris.'
+        ],
+        'actionCards' => [
+            [
+                'title' => 'Felis amet orci diam at',
+                'buttonText' => 'History',
+                'buttonLink' => '/history',
+                'color' => 'blue'
+            ],
+            [
+                'title' => 'Praesent in eros nam quis ultrices',
+                'buttonText' => 'Outline',
+                'buttonLink' => '/outline',
+                'color' => 'green'
+            ]
+        ]
+    ]
+];
