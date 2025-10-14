e441e1085686bbf74e7cc344101ddd0248cc1520
Dinura Ransika <ransikadinura@gmail.com>
2025-10-14 15:46:40 +0530

    feat: add new images

--- data/company.php
+++ data/company.php
@@ -8,7 +8,7 @@ return [
             'Lorem ipsum dolor sit amet consectetur. Et interdum arcu gravida urna viverra bibendum habitant placerat nec justo mollis. Quis elegiaque pellentesque vitae et arcu interdum viam lectus. Dictum imperdiet quam eu amet. Vivamus felis sollicituaque et id non ut. Etiam congue consectetur nulla amet et. Nisl ut eros risique quam pellentesque nunc sed.',
             'Nunc omcare sit aliquet arroe omcare molestiae risus vel blandit. Velit incididunt tempus vel modi gravida eget malesanda solitudinis. Ut et sed molestie vehicula defendit. Faucibus suscipit sed mattis risl et venenatis maecenas volutpat. Aliquam tempus velit quis sed libero sem nunc. Phasellut gravida neque tristique enim. Curabitur montes sed molestae tacuila. Velit augue proin nis nunc at amet non in pharetra. A vitrue arco consectetur risus enim eget quis sit. In viverra proin risus augue aem. Nec sed et enim et eget. Suscipit tristique vivamus vulputate donce.'
         ],
-        'image' => '/images/project-2.png'
+        'image' => 'images/company.png'
     ],
     'history' => [
         [
