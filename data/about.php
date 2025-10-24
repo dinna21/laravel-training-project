b9cc403e750c3cfccfd19c21887512fa4699cb10
Dinura Ransika <ransikadinura@gmail.com>
2025-10-24 08:21:47 +0530

    feat: add two images for the image dark and light

--- data/about.php
+++ data/about.php
@@ -1,5 +1,4 @@
 <?php
-// filepath: e:\laravel-training-project\data\about.php
 
 return [
     'project' => [
@@ -11,6 +10,10 @@ return [
     'about' => [
         'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus. At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
         'images' => [
+            '/images/image (2).png',
+            '/images/image 2.png'
+        ],
+        'imagesDark' => [
             '/images/image (3).png',
             '/images/image (12).png'
         ]
@@ -19,8 +22,12 @@ return [
         'description' => 'Lorem ipsum dolor sit amet consectetur. Congue ac enim urna amet mi. Semper enim in arcu est adipiscing etiam habitant suspendisse. Odio urna at sit leo erat nibh integer. Dictum faucibus turpis ante pulvinar netus.',
         'additionalInfo' => 'At nunc egestas adipiscing aliquet ac. Dui tincidunt vel odio malesuada tortor eget malesuada pellentesque etiam. Senectus tristique commodo vitae aliquam tortor quis adipiscing et.',
         'images' => [
-            '/images/image (3).png',
+            '/images/image (2).png',
             '/images/image 2.png'
+        ],
+        'imagesDark' => [
+            '/images/image (3).png',
+            '/images/image (12).png'
         ]
     ]   
 ];
