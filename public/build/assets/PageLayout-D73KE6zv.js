ebadb116047889b7b564c0e4aeb92b9911e11284
Dinura Ransika <ransikadinura@gmail.com>
2025-11-08 18:32:00 +0530

    feat: npm run build

---
+++ public/build/assets/PageLayout-D73KE6zv.js
@@ -0,0 +1,2 @@
+import{j as r}from"./app-BeMEZRHO.js";import{H as o,F as s}from"./Footer-CliStf2s.js";function n({children:t,className:a=""}){return r.jsxs("div",{className:`bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300 ${a}`,children:[r.jsx(o,{}),t,r.jsx(s,{})]})}export{n as P};
+//# sourceMappingURL=PageLayout-D73KE6zv.js.map
