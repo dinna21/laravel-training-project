527fb3c31ed951e9fdb635ed90309033057a40d8
Dinura Ransika <ransikadinura@gmail.com>
2025-11-08 15:27:01 +0530

    feat: run npm run build

---
+++ public/build/assets/PageLayout-78fPR-T0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CqdlXMxe.js";import{H as o,F as s}from"./Footer-BcZqJXV-.js";function n({children:t,className:a=""}){return r.jsxs("div",{className:`bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300 ${a}`,children:[r.jsx(o,{}),t,r.jsx(s,{})]})}export{n as P};
